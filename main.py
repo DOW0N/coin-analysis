ee924da9105057173a147e094f74c2aef656a1f6
DOWON <152825553+DOW0N@users.noreply.github.com>
2025-01-03 00:10:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import numpy as np
 import matplotlib.font_manager as fm
 
 # 한글 폰트 설정 (Windows 환경에 맞게 설정)
-font_path = 'C:/Windows/Fonts/malgun.ttf'  # 또는 적합한 한글 폰트 경로로 설정
+font_path = 'C:/Windows/Fonts/malgun.ttf'  
 font_prop = fm.FontProperties(fname=font_path)
 
 # MySQL 데이터베이스 연결
-engine = create_engine('mysql+pymysql://root:kmo665100!!@localhost/crypto_db')
+engine = create_engine('mysql+pymysql://root:password@localhost/crypto_db')
 
 # 데이터 조회 쿼리
 query_crypto = "SELECT * FROM crypto_data"
@@ -35,7 +35,7 @@ bitcoin_price_df['timestamp'] = pd.to_datetime(bitcoin_price_df['timestamp'])
 
 # 첫 번째 그래프: 비트코인 가격
 fig1, ax1 = plt.subplots(figsize=(10, 6))
-ax1.set_title('비트코인 가격', fontproperties=font_prop)  # 제목 한글 설정
+ax1.set_title('비트코인 가격', fontproperties=font_prop)  
 bitcoin_price_df.set_index('timestamp')['price'].plot(label='비트코인 가격', ax=ax1, color='r')
 
 ax1.set_xlabel('날짜', fontproperties=font_prop)  # x축 레이블 한글 설정
@@ -69,7 +69,7 @@ predicted_prices = model.predict(future_dates_int)
 
 # 예측 결과 그래프
 fig2, ax2 = plt.subplots(figsize=(10, 6))
-ax2.set_title('비트코인 시세 예측 (2025)', fontproperties=font_prop)  # 제목 한글 설정
+ax2.set_title('비트코인 시세 예측 (2025)', fontproperties=font_prop)  
 
 # 예측된 가격을 플로팅
 ax2.plot(future_dates, predicted_prices, label='예측된 가격', color='green')
